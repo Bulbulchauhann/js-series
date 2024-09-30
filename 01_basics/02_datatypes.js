0c522bd2638d6001a7977406fe78a3dfa5833876
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-09-30 16:40:59 +0530

    updates

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,12 @@
 "use strict"; //treat all JS code as new version
 
-// alert (3 + 3) won't work in node js
+// alert (3 + 3) won't work in node js, but in browser
+
+// console.log(3+3); console.log("blu") //not a good practice
+
+// console.log(3
+//     +3
+// )              // not good for code readability
+
+console.log("blu")
 
