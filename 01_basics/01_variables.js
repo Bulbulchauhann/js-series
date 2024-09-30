c90973f5513609736aa9ad79e153fcf74ce8ea52
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-09-30 15:45:04 +0530

    updates

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accId = 123
+let accEmail = "bulbul@gmail.com"
+var accPassword = "123"
+accCity = "blr"
+let accState;
+
+// accId= 1234 //not allowed(const)
+accEmail = "bulbul22@gmail.com"
+accPassword = "1234"
+accCity = "blrr"
+
+console.log(accId)
+console.table([accEmail, accPassword, accCity, accState])
+
+/*
+Prefer not
+*/
