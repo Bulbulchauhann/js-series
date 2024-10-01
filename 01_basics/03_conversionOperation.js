79db6b04c99eea1403fd8b83e6efccf23bb8b0e9
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-01 18:28:51 +0530

    Numbers in JavaScript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+let score = undefined;
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,6 +6,8 @@ console.log(typeof (score));
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber); //33 when "33"
-console.log(valueInNumber); //NaN when "33"(Not a number - NaN)
+console.log(valueInNumber); //NaN when "33abc"(Not a number - NaN)
+console.log(valueInNumber); // 0 when null 
+console.log(valueInNumber); //Nan when undefined
 
-
+//true => 1/false => 0
---
+++ 06_num_and_math.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(300)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); //float
+
+const otherNumber = 253.94632;
+console.log(otherNumber.toPrecision(3));
+
+
+
