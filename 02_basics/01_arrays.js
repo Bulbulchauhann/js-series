217c389320e4400ad672ff8e7aa6bc6209637d85
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-02 10:38:58 +0530

    updates

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,7 +3,8 @@
 const myArray = [0, 1, 2, 3, 4, 5]
 const myHeroes = ["Flying Jatt", "G One", "shaktiman"]
 
-console.log(myArray[0]);
+console.log(myArray[1]);
 console.log(myHeroes);
 
-
+//array methods
+ 
