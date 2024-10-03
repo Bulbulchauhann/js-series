d29b74f45395ecd914c717a22e63255ca542cb51
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-03 10:12:50 +0530

    arrays in js

--- 02_basics/02-arrays.js
+++ 02_basics/02-arrays.js
@@ -1,5 +1,15 @@
 let drink = ["coffee", "tea", "water"];
 let food = ["pizza", "burger", "pasta"];
 
-drink.push(food); //will add array in array
+// drink.push(food); //will add food array as an array only in drink array
 console.log(drink);
+// console.log(drink[3][1]); //not a good practice to merge like this
+
+let both = drink.concat(food) //to merge values of to array
+console.log(both);
+
+const bothAgain = [...drink,...food] //spread out method
+console.log(bothAgain);
+
+
+
