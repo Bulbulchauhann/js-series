ebba55fc6b08b3661ba18abc88c929d9b621fff1
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-03 10:21:04 +0530

    array updates

--- 02_basics/02-arrays.js
+++ 02_basics/02-arrays.js
@@ -11,5 +11,23 @@ console.log(both);
 const bothAgain = [...drink,...food] //spread out method
 console.log(bothAgain);
 
+const another_array = [1, 2, 3, [4, 5, 6], [6, 7, [9, 7]]]
+const another_array2 = [another_array.flat(2)] //to flat out level of array
+console.log(another_array2); 
+
+console.log(Array.isArray("rahul")); //to check if the given value is array or not
+console.log(Array.from("rahul")); //to make that value an array
+console.log(Array.from({name : "rahul"})); //gets confused, interesting
+
+let num1 = 100
+let num2 = 200
+let num3 = 300
+
+console.log(Array.of(num1, num2, num3)); //to make array from key value pair
+
+
+
+
+
 
 
