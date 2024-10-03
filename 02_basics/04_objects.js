2474c35b784166ca0b51e8f7e730aa65a988c5cd
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-03 11:31:52 +0530

    updates in objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -2,4 +2,32 @@
 const twtUser = new Object()
 const twtUser1 = {}
 
-console.log(twtUser, twtUser1);
+// console.log(twtUser, twtUser1);
+
+twtUser.id = "123abc"
+twtUser.name = "Amy"
+twtUser.isLoggedIn = false
+
+console.log(twtUser);
+
+const regUser = {
+    email : "amy22@gmail.com",
+    fullname : {
+        userFullName :{
+            firstName : "Amy",
+            lastName : "Peralta"
+        }
+    }
+}
+
+console.log(regUser.fullname.userFullName.firstName);
+
+//option chaining - used mostly when data is coming from api,
+//and not sure if it's there or not, to avoid throwing any error
+console.log(regUser.fullname?.userFullName.firstName); 
+
+//combing arrays
+
+
+
+
