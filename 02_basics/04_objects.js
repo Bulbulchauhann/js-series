bb399879a0f5c86b9cf2cf04f0862f110d0300e3
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-03 11:38:11 +0530

    udpated in objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,6 +27,22 @@ console.log(regUser.fullname.userFullName.firstName);
 console.log(regUser.fullname?.userFullName.firstName); 
 
 //combing arrays
+const obj1 = {1:"bulbul", 2 : "Rahul", 3 : "Pooja"}
+const obj2 = {4 : "Vivek", 5 : "Sumit", 6 : "Mayank"}
+
+console.log(({obj1, obj2})); //problematic, array in array
+
+const obj3 = Object.assign(obj1, obj2) //all values going to obj1
+const obj4 = Object.assign({}, obj1, obj2) //{} is target, and obj1, obj2 are source
+console.log(obj3);
+console.log(obj4); //option parameter, but recommended, values get combined, gives exact value
+
+
+
+
+
+
+
 
 
 
