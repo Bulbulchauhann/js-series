f0d3ab82ae852cf6716d34bbfe90417041ec98c0
Bulbulchauhann <chauhanbulbul8@gmail.com>
2024-10-04 16:06:27 +0530

    updates in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,9 +1,31 @@
 //functions in javascript
 
-function sayMyName(){
-    console.log("Bulbul");
-    
+function sayMyName() {
+  console.log("Bulbul");
 }
 
 //calling function
 sayMyName();
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// function add(num1, num2) {
+//   console.log("Bulbul"); //it will be executed here
+//   let result = num1 + num2;
+//   return result;
+// //   console.log("Bulbul"); //will never execute because there's way to execute function
+  
+// }
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+
+const result = add(3, 1000);
+console.log(result);
+
+add(12, "a");
+add(12, null);
